dfb8e91055ae74e91fdd27b4a9009042a0e33da5
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-05-16 09:11:47 +0100

    initial commit

---
+++ portfolio/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'portfolio/home.html')
+
+def about(request):
+    return render(request, 'portfolio/about.html')
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/projects.html', {'projects': projects})
