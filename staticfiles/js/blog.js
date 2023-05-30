466933288623dc33dcef787becd02e02eb37f326
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-05-30 21:09:09 +0100

    bug fix 4

--- staticfiles/js/blog.js
+++ staticfiles/js/blog.js
@@ -1,15 +1,2 @@
 // blog/static/js/blog.js
 
-window.onload = function() {
-    var authorLinks = document.getElementsByClassName('author-link');
-
-    for (let i = 0; i < authorLinks.length; i++) {
-        authorLinks[i].addEventListener('click', function(e) {
-            if('{{ user.is_authenticated }}' === 'False') {
-                e.preventDefault();
-                var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-                myModal.show();
-            }
-        });
-    }
-}
